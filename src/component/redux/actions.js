291469a412450f40f9cf803411e75b85fe311a2c
DLY <137272885+YGeorgeDL@users.noreply.github.com>
2023-07-12 22:40:14 +0800

    React,JS,HTML,CSS

---
+++ src/component/redux/actions.js
@@ -0,0 +1,9 @@
+const ACTIONS = {
+    ADD_DIGHT: "add-digit",
+    DELETE_DIGHT: "delete-digit",
+    CHOOSE_OPERATION: "choose-operation",
+    CLEAR: "clear",
+    EVALUATE:"evaluate"
+}
+
+export default ACTIONS;
